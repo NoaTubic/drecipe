b9d2f06377c574627dae9d3ed1bd850e89f19ae9
Noa Tubic <noa@revlum.com>
2023-10-09 10:41:30 +0200

    Fixed meal time card datetime updates in real time

--- lib/features/discover_recipes/domain/notifiers/meal_time/meal_time_notifier.dart
+++ lib/features/discover_recipes/domain/notifiers/meal_time/meal_time_notifier.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:drecipe/features/common/ui/assets/image_assets.dart';
 import 'package:drecipe/features/discover_recipes/domain/notifiers/meal_time/meal_time_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,49 +11,53 @@ final mealTimeNotifierProvider =
 
 class MealTimeNotifier extends StateNotifier<MealTimeState> {
   MealTimeNotifier() : super(MealTimeState.initial());
+  late final Timer _timer;
 
   Future<void> getMealTypeAndTime() async {
     state = state.copyWith(isLoading: true);
 
-    DateTime dateTime = DateTime.now();
-    String minute =
-        dateTime.minute > 9 ? '${dateTime.minute}' : '0${dateTime.minute}';
-    String hour = dateTime.hour > 9 ? '${dateTime.hour}' : '0${dateTime.hour}';
-    String currentTime = '$hour : $minute';
-    Future.delayed(const Duration(seconds: 3), () {
-      state = state.copyWith(currentTime: currentTime, isLoading: false);
-    });
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      DateTime dateTime = DateTime.now();
+      String minute =
+          dateTime.minute > 9 ? '${dateTime.minute}' : '0${dateTime.minute}';
+      String hour =
+          dateTime.hour > 9 ? '${dateTime.hour}' : '0${dateTime.hour}';
+      String currentTime = '$hour : $minute';
+      Future.delayed(const Duration(seconds: 3), () {
+        state = state.copyWith(currentTime: currentTime, isLoading: false);
+      });
 
-    if (dateTime.hour >= 6 && dateTime.hour < 12) {
-      state = state.copyWith(
-        currentTime: currentTime,
-        mealType: 'breakfast',
-        mealTypeIcon: ImageAssets.icBreakfast,
-      );
-    } else if (dateTime.hour >= 12 && dateTime.hour < 16) {
-      state = state.copyWith(
-        currentTime: currentTime,
-        mealType: 'lunch',
-        mealTypeIcon: ImageAssets.icLunch,
-      );
-    } else if (dateTime.hour >= 16 && dateTime.hour < 18) {
-      state = state.copyWith(
-        currentTime: currentTime,
-        mealType: 'snack',
-        mealTypeIcon: ImageAssets.icSnack,
-      );
-    } else if (dateTime.hour >= 18 && dateTime.hour < 23) {
-      state = state.copyWith(
-        currentTime: currentTime,
-        mealType: 'dinner',
-        mealTypeIcon: ImageAssets.icDinner,
-      );
-    } else {
-      state = state.copyWith(
-        currentTime: currentTime,
-        mealType: 'bed',
-        mealTypeIcon: ImageAssets.icBed,
-      );
-    }
+      if (dateTime.hour >= 6 && dateTime.hour < 12) {
+        state = state.copyWith(
+          currentTime: currentTime,
+          mealType: 'breakfast',
+          mealTypeIcon: ImageAssets.icBreakfast,
+        );
+      } else if (dateTime.hour >= 12 && dateTime.hour < 16) {
+        state = state.copyWith(
+          currentTime: currentTime,
+          mealType: 'lunch',
+          mealTypeIcon: ImageAssets.icLunch,
+        );
+      } else if (dateTime.hour >= 16 && dateTime.hour < 18) {
+        state = state.copyWith(
+          currentTime: currentTime,
+          mealType: 'snack',
+          mealTypeIcon: ImageAssets.icSnack,
+        );
+      } else if (dateTime.hour >= 18 && dateTime.hour < 23) {
+        state = state.copyWith(
+          currentTime: currentTime,
+          mealType: 'dinner',
+          mealTypeIcon: ImageAssets.icDinner,
+        );
+      } else {
+        state = state.copyWith(
+          currentTime: currentTime,
+          mealType: 'bed',
+          mealTypeIcon: ImageAssets.icBed,
+        );
+      }
+    });
   }
 }
